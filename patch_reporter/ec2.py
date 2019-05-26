7436aed1e25ea2427de471d5003c6db3eff96e77
Song Jin <songjin@hotmail.com>
2019-05-26 22:13:38 +1000

    update design

--- patch_reporter/ec2.py
+++ patch_reporter/ec2.py
@@ -1,21 +1,14 @@
-# -*- coding: utf-8 -*-
-
 """
 patch-reporter - ec2_instance_queries
 ~~~~~~~~~~~~~~
 This module provides utility to query ec2 instances within account
+and output tags in list of dicts format [dict1, dict2, ... ,dictn]
 """
 import boto3
 
-def convert_tags_to_dict(tag_list):
-    '''
-        input: [{'Key': 'key2', 'Value': 'val2'}, {'Key': 'key1', 'Value': 'val1'}]
-        output: {key1: val1, key2: val2}
-    '''
-
 
 def list_instance_by_tags(tag_key,tag_value):
-    '''return instances with input tag-value combination'''
+    '''return filtered instances with specified tag-value combination'''
     ec2 = boto3.client("ec2", region_name = 'ap-southeast-2')
     reservations = ec2.describe_instances()['Reservations']
     instances = []
@@ -27,9 +20,21 @@ def list_instance_by_tags(tag_key,tag_value):
     return instances
 
 def list_instance_tags(instance):
-    ''' given an instance, list all tags'''
+    ''' given an instance, list all its tags'''
     pass
 
+def convert_tags_to_dict(tag_list):
+    '''
+        given a list of tags in AWS format,
+        return well-formatted dict {key1: val1, key2: val2}
+        input: [
+                {'Key': 'key2', 'Value': 'val2'},
+                {'Key': 'key1', 'Value': 'val1'}
+            ]
+        output: {key1: val1, key2: val2}
+    '''
+    pass
 
-found_instances = list_instance_by_tags(tag_key='CostCentre', tag_value='V_ABC')
-print(found_instances['InstanceId'], found_instances['ImageId'])
+if __name__ == '__main__':
+    found_instances = list_instance_by_tags(tag_key='CostCentre', tag_value='V_ABC')
+    print(found_instances['InstanceId'], found_instances['ImageId'])
---
+++ patch_reporter/hip_website.py
@@ -0,0 +1,15 @@
+"""
+patch-reporter - hip_website_queries
+~~~~~~~~~~~~~~
+objective:
+"""
+
+def get_data_csv():
+    pass
+
+def filter_data_with_tag(tag_key, tag_value):
+    pass
+
+def form_output():
+    '''given a row of data, return desired formatted output'''
+    pass
